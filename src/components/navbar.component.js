ae112de732f95e8ec65ec0356a38ff6aba21ef56
Jak <jaksawesome10@gmail.com>
2020-08-04 12:42:17 -0400

    container and stuff

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -7,18 +7,19 @@ export default class navbar extends Component{
         return (
             <nav className='navbar navbar-dark bg-dark navbar-expand-ld'>
                 <Link to='/' className='navbar-brand'>ExerTracker</Link>
-                <div classname='collapse navbar-collapse'></div>
-                <ul className='navbar-nav mr-auto'>
-                    <li className='navbar-item'>
-                        <Link to='/' className='nav-link'>Exercises</Link>
-                    </li>
-                    <li className='navbar-item'>
-                        <Link to='/create' className='nav-link'>Create Exercise Log</Link>
-                    </li>
-                    <li className='navbar-item'>
-                        <Link to='/user' className='nav-link'>Create User</Link>
-                    </li>
-                </ul>
+                <div classname='collapse navbar-collapse'>
+                    <ul className='navbar-nav mr-auto'>
+                        <li className='navbar-item'>
+                            <Link to='/' className='nav-link'>Exercises</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/create' className='nav-link'>Create Exercise Log</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/user' className='nav-link'>Create User</Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         );
     }
